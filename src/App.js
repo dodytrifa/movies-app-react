439d76799aa2b07743097bcbe4ec9ddc74d4834d
dodytrifa <dody.trifa@gmail.com>
2021-04-21 17:26:10 +0700

    set API key

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
+import SingleMovie from './SingleMovie'
 import Movie from './SingleMovie'
 
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/movies/:id" children={<Movie />}>
+      </Route>
+    </Switch>
+  ) 
 }
 
 export default App
