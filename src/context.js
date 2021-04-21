837d15d1e2b6aff174e345692388ab92d3d280ca
dodytrifa <dody.trifa@gmail.com>
2021-04-21 18:53:35 +0700

    resolve key

--- src/context.js
+++ src/context.js
@@ -1,6 +1,9 @@
 import React, { useState, useContext, useEffect } from 'react'
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
+console.log(API_ENDPOINT);
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
@@ -26,7 +29,7 @@ const AppProvider = ({ children }) => {
 
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
-// make sure use
+// make sure use global context
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
